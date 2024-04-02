cbb37e1e160be00d76d4205c76ef499dfabc5133
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-02 11:27:22 +0200

    Service interface

--- ExpenseTracker_Backend/src/main/java/com/Daniel/ExpenseTracker_Backend/service/ExpenseService.java
+++ ExpenseTracker_Backend/src/main/java/com/Daniel/ExpenseTracker_Backend/service/ExpenseService.java
@@ -1,4 +1,14 @@
 package com.Daniel.ExpenseTracker_Backend.service;
 
+import com.Daniel.ExpenseTracker_Backend.model.Expense;
+
+import java.util.List;
+
 public interface ExpenseService {
+
+    public Expense saveExpense(Expense expense);
+    public List<Expense> getAllExpense();
+    public Expense getExpenseById(Long id);
+    public String deleteExpense(Long id);
+    public Expense editEcpense(Expense expense, Long id);
 }
--- ExpenseTracker_Backend/src/main/java/com/Daniel/ExpenseTracker_Backend/service/ExpenseServiceImpl.java
+++ ExpenseTracker_Backend/src/main/java/com/Daniel/ExpenseTracker_Backend/service/ExpenseServiceImpl.java
@@ -1,4 +1,33 @@
 package com.Daniel.ExpenseTracker_Backend.service;
 
-public class ExpenseServiceImpl {
+import com.Daniel.ExpenseTracker_Backend.model.Expense;
+
+import java.util.List;
+
+public class ExpenseServiceImpl implements ExpenseService{
+
+    @Override
+    public Expense saveExpense(Expense expense) {
+        return null;
+    }
+
+    @Override
+    public List<Expense> getAllExpense() {
+        return null;
+    }
+
+    @Override
+    public Expense getExpenseById(Long id) {
+        return null;
+    }
+
+    @Override
+    public String deleteExpense(Long id) {
+        return null;
+    }
+
+    @Override
+    public Expense editEcpense(Expense expense, Long id) {
+        return null;
+    }
 }
