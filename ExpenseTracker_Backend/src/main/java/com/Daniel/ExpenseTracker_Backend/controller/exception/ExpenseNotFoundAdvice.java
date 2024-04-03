396f76106039f4a960b235f3c0e9def5f1d3358e
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-03 15:06:29 +0200

    Not found advice

--- ExpenseTracker_Backend/src/main/java/com/Daniel/ExpenseTracker_Backend/controller/exception/ExpenseNotFoundAdvice.java
+++ ExpenseTracker_Backend/src/main/java/com/Daniel/ExpenseTracker_Backend/controller/exception/ExpenseNotFoundAdvice.java
@@ -1,4 +1,24 @@
 package com.Daniel.ExpenseTracker_Backend.controller.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class ExpenseNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> exceptionHandler(ExpenseNotFoundException exception){
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+
+        return errorMap;
+    }
 }
